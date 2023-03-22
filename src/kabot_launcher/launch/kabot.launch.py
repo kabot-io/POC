44b9a705f7b37a7f3025d6f7f052e4bc554eaa99
Jakub <jakubkolasa99@gmail.com>
2023-03-22 12:40:02 +0100

    Aruco detection. Usb camera config files. Improved kabot.launch

--- src/kabot_launcher/launch/kabot.launch.py
+++ src/kabot_launcher/launch/kabot.launch.py
@@ -1,6 +1,10 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
 def generate_launch_description():
 
     kabot_agent = Node(
@@ -10,12 +14,42 @@ def generate_launch_description():
         arguments=["udp4", "-p", "8888", "-v6"]
     )
 
+    kabot_launcher_dir = get_package_share_directory('kabot_launcher')
+    camera_params_path = os.path.join(
+        kabot_launcher_dir,
+        'config',
+        'camera_params.yaml'
+    )
+
+    usb_camera_node = Node(
+        package='usb_cam',
+        executable='usb_cam_node_exe',
+        name='usb_camera_node',
+        parameters=[camera_params_path]
+    )
+
+    aruco_detection_node = Node(
+        package='ros2_aruco',
+        executable='aruco_node',
+        name='aruco_detection_node',
+        emulate_tty=True,
+        parameters=[{'image_topic':'/image_raw'}, {'camera_info_topic':'/camera_info'}],
+    )
+
     kabot_app = Node(
         package='kabot_app',
         executable='kabot_app',
     )
 
+    rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+    )
+
     return LaunchDescription([
         kabot_agent,
-        kabot_app
+        usb_camera_node,
+        aruco_detection_node,
+        # kabot_app,
+        rviz2,
     ])
